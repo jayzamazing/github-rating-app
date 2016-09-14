802eed9890d4c0b0f257ac5cdb3c73052a339d5f
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-09-14 08:53:59 -0400

    Added public/js/repository-list.js.

---
+++ public/js/repository-list.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var Repository = require('./repository');
+
+var RepositoryList = React.createClass({
+  addRepository: function() {
+    var repositoryName = this.refs.repositoryName.value;
+    //TODO: add repository to state
+  },
+  render: function() {
+    var repositories = this.props.repositories.map(function(repository) {
+      return <Repository repository={repository} key={repository.name} />;
+    });
+    return (
+            <div className="repository-list">
+                {repositories}
+                <input type="text" ref="repositoryName" />
+                <button type="button" onClick={this.addRepository}>
+                    Add repository
+                </button>
+            </div>
+    );
+  }
+});
+module.exports = RepositoryList;
