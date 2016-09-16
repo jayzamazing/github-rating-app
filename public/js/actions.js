ea67834c0a578a9b02c2b9ad8bafa4f9d824ad18
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-09-16 09:55:15 -0400

    Added fetchdescriptionerror and fetchdescriptionerror in public/js/actions.js.

--- public/js/actions.js
+++ public/js/actions.js
@@ -19,7 +19,28 @@ var rateRepository = function(repository, rating) {
     rating: rating
   };
 };
+var FETCH_DESCRIPTION_SUCCESS = 'FETCH_DESCRIPTION_SUCCESS';
+var fetchDescriptionSuccess = function(repository, description) {
+    return {
+        type: FETCH_DESCRIPTION_SUCCESS,
+        repository: repository,
+        description: description
+    };
+};
 
+var FETCH_DESCRIPTION_ERROR= 'FETCH_DESCRIPTION_ERROR';
+var fetchDescriptionError = function(repository, error) {
+    return {
+        type: FETCH_DESCRIPTION_ERROR,
+        repository: repository,
+        error: error
+    };
+};
+
+exports.FETCH_DESCRIPTION_SUCCESS = FETCH_DESCRIPTION_SUCCESS;
+exports.fetchDescriptionSuccess = fetchDescriptionSuccess;
+exports.FETCH_DESCRIPTION_ERROR = FETCH_DESCRIPTION_ERROR;
+exports.fetchDescriptionError = fetchDescriptionError;
 exports.ADD_REPOSITORY = ADD_REPOSITORY;
 exports.addRepository = addRepository;
 exports.RATE_REPOSITORY = RATE_REPOSITORY;
